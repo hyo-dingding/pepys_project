2f02ec24adcff8907ff39c93a82fd624b8361e35
Welsh Zuckerberg <welshzucker@gmail.com>
2024-10-16 18:45:47 +0900

    calenderv1

--- frontend/app/src/navigation/Folder.js
+++ frontend/app/src/navigation/Folder.js
@@ -2,20 +2,18 @@ import { View, Text, StyleSheet } from "react-native";
 import Header from "../screens/Header";
 
 export default function Folder() {
-    return (
-        <View style={{ flex: 1 }}>
-            <Header title="Folder" />
+  return (
+    <View style={{ flex: 1 }}>
+      <Header title="Folder" />
 
-            <View
-                style={{
-                    flex: 1,
-                    alignContent: "center",
-                    justifyContent: "center",
-                    backgroundColor: "white",
-                }}
-            >
-             
-            </View>
-        </View>
-    );
+      <View
+        style={{
+          flex: 1,
+          alignContent: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}
+      ></View>
+    </View>
+  );
 }
