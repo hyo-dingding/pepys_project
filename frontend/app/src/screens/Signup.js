e5016244a0bf717daf53f9d545198fed765b94a0
dongjoo <cherry5895@naver.com>
2024-10-29 09:47:52 +0900

    Feature_로그인/로그아웃api연동_비밀번호유효성수정

--- frontend/app/src/screens/Signup.js
+++ frontend/app/src/screens/Signup.js
@@ -164,17 +164,14 @@ const SignUp = () => {
 
   const handleSignUp = async () => {
     try {
-      const response = await axios.post(
-        "https://5ada-59-31-88-239.ngrok-free.app/user",
-        {
-          email: email,
-          password: password,
-          password_retype: password,
-          name: name,
-          nationality: nationality,
-          work_title: workTitle,
-        }
-      );
+      const response = await axios.post("${NGROK_URL}/user", {
+        email: email,
+        password: password,
+        password_retype: password,
+        name: name,
+        nationality: nationality,
+        work_title: workTitle,
+      });
 
       console.log(response.data);
       console.log(response.status);
